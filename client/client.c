be5de0fa2df297e551e1032e0175363e0ff1fa9a
Eric Kim <kimeric657@gmail.com>
2025-03-06 12:46:22 -1000

    initial commit

---
+++ client/client.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 25518
+#define BUFFER_SIZE 1024
+
+int main()
+{
+    int client_fd;
+    struct sockaddr_in server_addr;
+    char buffer[BUFFER_SIZE] = {0};
+
+    // Create Socket
+    client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_fd < 0)
+    {
+        perror("Socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Connect to Server
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+
+    if (connect(client_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("Connected to server!\n");
+
+    // Receive File
+    FILE *file = fopen("file_client.html", "w");
+    int bytes;
+    while ((bytes = recv(client_fd, buffer, BUFFER_SIZE, 0)) > 0)
+    {
+        fwrite(buffer, 1, bytes, file);
+    }
+    printf("File received successfully.\n");
+    fclose(file);
+
+    // Close Connection
+    close(client_fd);
+    return 0;
+}
---
+++ server/server.c
@@ -0,0 +1,58 @@
+#include <stdio.h>     // printf, perror
+#include <stdlib.h>    // exit()
+#include <string.h>    // strlen()
+#include <unistd.h>    // close()
+#include <arpa/inet.h> // socket(), bind(), listen(), accept(), etc.
+
+#define PORT 25518 // The port nuymber the server will listen on
+#define BUFFER_SIZE 1024
+
+int main()
+{
+    int server_fd, client_fd;       // Initialize server and client file descriptors
+    struct sockaddr_in server_addr; // Initialize server address structure
+    char buffer[BUFFER_SIZE] = {0};
+
+    // Create a socketn (IPv4, TCP)
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Bind Socket
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    listen(server_fd, 1);
+
+    printf("Server listening on port %d\n", PORT);
+
+    // Accept Connection
+    client_fd = accept(server_fd, (struct sockaddr *)NULL, NULL);
+    if (client_fd < 0)
+    {
+        perror("accept failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("Client connected\n");
+
+    // Send File
+    FILE *file = fopen("server_file.html", "r");
+    int bytes;
+    while ((bytes = fread(buffer, 1, BUFFER_SIZE, file)) > 0)
+    {
+        send(client_fd, buffer, bytes, 0);
+    }
+    printf("File sent successfully\n");
+    fclose(file);
+
+    // Close the connection
+    close(client_fd);
+    close(server_fd);
+
+    return 0;
+}
